d6492ea9d78dee0ccfe47f216b566d8b2ff05347
Vinel-Ivan <vinel.ivan051@gmail.com>
2021-03-15 13:13:30 +0300

    feat: add min max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  } 
+  else {
+    return minValue = Math.min.apply(null,array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  } 
+ else {
+   return maxValue = Math.max.apply(null,array);
+ }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  } 
+  else {
+  const sumValue = array.reduce((sum, current) => sum + current, 0)
+  avgValue = (sumValue / array.length)
+  return avgValue;
+  }
 }
